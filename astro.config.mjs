397c466b067fe3d72e1a8ade17e139bfea70231d
Matees <mateespublicprofile@gmail.com>
2022-12-06 23:09:21 -0500

    created v1?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  site: 'https://matees.net',
+  integrations: [mdx(), sitemap(),]
 });
